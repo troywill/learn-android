edc899f734ee5a52120c87b159b840e84426b60a
Troy Will <troydwill@gmail.com>
2013-04-20 15:00:53 -0700

    tools/uninstall-app-with-adb.sh

---
+++ tools/uninstall-app-with-adb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# -d directs command to the only connected USB device
+#           returns an error if more than one USB device is present.
+# -e directs command to the only running emulator.
+
+adb -d uninstall com.example.myfirstapp
+adb -e uninstall com.example.myfirstapp
